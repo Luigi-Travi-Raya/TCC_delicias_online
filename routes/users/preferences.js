7e843ad8aea54c77ebbbe302cf58c7b3dea0bbc9
Luigi-Travi-Raya <traviraya@gmail.com>
2022-11-17 12:09:58 -0300

    edidata frontend working

--- routes/users/preferences.js
+++ routes/users/preferences.js
@@ -8,7 +8,6 @@ const fs = require('fs');
 
 const Edit = {
     route:(req,res,edit_pswrd)=>{
-        console.log(edit_pswrd);
         if(edit_pswrd){
             res.render('preferencia', {editPassword: true, isLogged:req.session.isLogged});
             console.log("Acessando preferencia.pug")
@@ -18,7 +17,7 @@ const Edit = {
             else{
                 tb_usuarios.findAll({ where:{id_usuario: req.session.userId}}).then(result=>{
                     req.session.userEmail = result[0]["email_usuario"];
-                    res.render('preferencia', {userName:req.session.username, userEmail:req.session.userEmail, isLogged:req.session.isLogged, editPassword: false});
+                    res.render('preferencia', {userName:req.session.username, userEmail:req.session.userEmail, isLogged:req.session.isLogged, editPassword: false, erro: req.session.errEdit});
                     console.log("Acessando edita.pug")
                 })
             }
@@ -38,31 +37,34 @@ const Edit = {
                         req.session.errEdit = "user_taken";
                         return res.redirect('/preferences');
                     }
-                })
-                tb_usuarios.findAll({where:{email_usuario: fields.email }}).then(result=>{
-                    if(result != "" && result[0]['email_usuario'] != fields.email){
-                        req.session.errEdit = "email_exists";
-                        return res.redirect('/preferences');
-                    }
-                })
-                let imgPath = files.img.filepath;
-                let hash = crypto.createHash('md5').update(Date.now().toString()).digest('hex');
-                let imgName = hash + '.' + files.img.mimetype.split('/')[1];
 
-                let newImgPath = path.join(__dirname, '../../public/img' , imgName);
-                fs.rename(imgPath, newImgPath, (err) => { if (err) throw err})
+                    tb_usuarios.findAll({where:{email_usuario: fields.email }}).then(result=>{
+                        console.log(result[0]['email_usuario'])
+                        if(result != "" && result[0]['id_usuario'] != req.session.userId){
+                            
+                            req.session.errEdit = "email_exists";
+                            return res.redirect('/preferences');
+                        }
+                        let imgPath = files.img.filepath;
+                        let hash = crypto.createHash('md5').update(Date.now().toString()).digest('hex');
+                        let imgName = hash + '.' + files.img.mimetype.split('/')[1];
 
-                    tb_usuarios.update({
-                        nome_usuario: fields.name,
-                        email_usuario: fields.email,
-                        nome_foto_usuario: imgName
-                    },{where:{id_usuario: req.session.userId}} 
-                    )
-                 res.redirect('/');        
-            })
+                        let newImgPath = path.join(__dirname, '../../public/img' , imgName);
+                        fs.rename(imgPath, newImgPath, (err) => { if (err) throw err})
+
+                            tb_usuarios.update({
+                                nome_usuario: fields.name,
+                                email_usuario: fields.email,
+                                nome_foto_usuario: imgName
+                            },{where:{id_usuario: req.session.userId}} 
+                            )
+                        res.redirect('/');        
+                    })
+                })
+        })
     },
     editPassword:(req,res)=>{
-        
+
     }
 }
 module.exports = Edit;
