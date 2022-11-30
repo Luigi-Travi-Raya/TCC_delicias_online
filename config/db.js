40787b47c123c5e469bfc558925a848bf970ccd8
Luigi-Travi_Raya <traviraya@gmail.com>
2022-11-30 15:04:56 -0300

    nto working

--- config/db.js
+++ config/db.js
@@ -2,8 +2,21 @@ const Sequelize = require('sequelize');
 const {config} = require('dotenv')
 config();
 
-const sequelize = new Sequelize(process.env.DBNAME, 'root', '',
-{dialect: 'mysql', host: process.env.DBHOST, query:{raw:true}}
+const sequelize = new Sequelize(
+    process.env.DBNAME,
+    process.env.DBUSER,
+    process.env.PASSWORD,
+    {dialect: 
+        process.env.DBDIALECT,
+        host: process.env.DBHOST,
+        encrypt:true
+    }, 
+    {dialectOption:
+        {ssl:
+            ca: fs.readFileSync("your_path_to_ca_cert_file_BaltimoreCyberTrustRoot.crt.pem")   
+        }
+        
+    } 
 );
 
 module.exports = sequelize;
